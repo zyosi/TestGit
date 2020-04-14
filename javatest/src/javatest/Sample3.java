221ab7702efdc5e4260afe21b30a8083d11d5a27
zyosi <tetu@tbr.t-com.ne.jp>
2020-04-14 15:47:54 +0900

    パラメータを変更した

--- javatest/src/javatest/Sample3.java
+++ javatest/src/javatest/Sample3.java
@@ -9,8 +9,8 @@ class Sample3
 		Iviecle [] ivc;
 		ivc = new Iviecle[2];
 		
-		ivc[0] = new Car2(12345, 20.5);
-		ivc[1] = new Plane2(6666);
+		ivc[0] = new Car2(12345, 25.5);
+		ivc[1] = new Plane2(6668);
 		
 		for (int i=0; i<ivc.length; i++) {
 				ivc[i].show();
